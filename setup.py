79a7e5bf0239608f2528c9b6fd330890e2b8a2df
thorwolpert <thor@wolpert.ca>
2022-01-09 00:33:49 -0800

    changed the setup

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
+# Copyright © 2019 Province of British Columbia.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Installer and setup for this module
+"""
+import ast
 from glob import glob
 from os.path import basename, splitext
+import re
+
 from setuptools import setup, find_packages
-from typing import Final
 
+_version_re = re.compile(r'__version__\s+=\s+(.*)')  # pylint: disable=invalid-name
 
-PACKAGE_NAME: Final = 'simple-cloudevent'
+with open('src/simple_cloudevent/__version__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(  # pylint: disable=invalid-name
+        f.read().decode('utf-8')).group(1)))
 
 
 def read_requirements(filename):
@@ -30,12 +50,20 @@ def read(filepath):
     return content
 
 
+REQUIREMENTS = read_requirements('requirements.txt')
+
 setup(
-    name=PACKAGE_NAME,
-    install_requires=read_requirements('requirements.txt'),
+    name="simple-cloudevent",
+    version=version,
+    author_email='thor@daxiom.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     include_package_data=True,
     license=read('LICENSE'),
     long_description=read('README.md'),
+    zip_safe=False,
+    install_requires=REQUIREMENTS,
     setup_requires=["pytest-runner", ],
     tests_require=["pytest", ],
 )
